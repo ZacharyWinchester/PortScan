375cb62d4fefa2c529eb83acda9693a91ceab797
ZacharyWinchester <zdnewinchester@gmail.com>
2023-10-25 10:32:58 -0400

    Update port.go
    Added some comments and logging functionality.

--- Port/port.go
+++ Port/port.go
@@ -1,33 +1,36 @@
-package port
+package port // Initializes the file as a package.
 
 import (
-	"net"
-	"strconv"
-	"time"
+	"net" // Golang portable interface for network input/output.
+	"strconv" // Implements conversions to and from string representations of basic data types.
+	"time" // Provides functionality for measuring and displaying time.
+	log "github.com/sirupsen/logrus" // Adds advanced logging functionality using the logrus package.
 )
 
-type ScanResult struct {
+type ScanResult struct { // Creates a structure to be called later for implimentation into an array. Allows the array to easily store both the port and state of the port in each element
 	Port  int
 	State string
 }
 
-func ScanPort(protocol, hostname string, port int) ScanResult {
+func ScanPort(protocol, hostname string, port int) ScanResult { // Function that takes a protocol, hostname, and port. Returns as the ScanResult structure.
 	result := ScanResult{Port: port}
-	address := hostname + ":" + strconv.Itoa(port)
-	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+	address := hostname + ":" + strconv.Itoa(port) // Takes the hostname (represented as an ip), concatinates a ':' (signifies a socket) to it, and then turns the port number into a string so that it can be concatinated to the rest of the address. Stores in address.
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second) /* DialTimeout is a function in the net package. It takes a protocol, address, and a timeout duration. It attempts to connect to the address
+ 									using the given protocol. Returns conn (which reads data from the connection), and error. If there is no error, then it returns nil for error.
+	  								conn and error are then assigned to conn and err in this function to be used later.*/
 
-	if err != nil {
-		result.State = "Closed"
-		return result
+	if err != nil { // If there is an error, run this:
+		result.State = "Closed" // Sets the state in this port's element to "Closed".
+		return result // Returns the element.
 	}
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-
+	defer func(conn net.Conn) { // Waits for surrounding functions to return before this function executes. Tries to close the connection that was established previously.
+		err := conn.Close() // Stores any produced errors from trying to close the connection.
+		if err != nil { // If there is an error, print that an error was encountered.
+			log.Warn("Connection close error")
 		}
 	}(conn)
-	result.State = "Open"
-	return result
+	result.State = "Open" // Should no errors be encountered, set the state atribute of the element in go to Open.
+	return result // Returns the element of the array.
 }
 
 func InitialScan(hostname string) []ScanResult {
