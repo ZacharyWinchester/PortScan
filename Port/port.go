810f5bdeee2d054a31bbd990bab118140dd8fc11
ZacharyWinchester <zdnewinchester@gmail.com>
2023-10-26 17:21:14 -0400

    Update port.go

--- Port/port.go
+++ Port/port.go
@@ -43,7 +43,7 @@ func ScanPort(protocol, hostname string, port int, wg *sync.WaitGroup) ScanResul
 }
 
 func InitialScan(hostname string) []ScanResult { // Takes an IP address as an argument, and returns an array
-	for i := 0; i <= 1024; i++ { // As long as i is less than or equal to 1024, run the following and increase i by one.
+	for i := 0; i <= 3; i++ { // As long as i is less than or equal to 1024, run the following and increase i by one.
 		wg.Add(1)
 		go func(port int) {
 			defer wg.Done()
