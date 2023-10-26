c75e7fc4b09c42f309c358c0968633a64d6db7c4
ZacharyWinchester <zdnewinchester@gmail.com>
2023-10-26 11:35:04 -0400

    Update port.go
    test

--- Port/port.go
+++ Port/port.go
@@ -14,7 +14,7 @@ type ScanResult struct { // Creates a structure to be called later for impliment
 }
 
 func ScanPort(protocol, hostname string, port int) ScanResult { // Function that takes a protocol, hostname, and port. Returns as the ScanResult structure.
-	result := ScanResult{Port: port} // Sets the Port element to the port number taken in by this function.
+	//result := ScanResult{Port: port} // Sets the Port element to the port number taken in by this function.
 	result := ScanResult{Protocol: protocol} // Sets the Protocol element to the protocol type taken in by this function.
 	address := hostname + ":" + strconv.Itoa(port) // Takes the hostname (represented as an ip), concatinates a ':' (signifies a socket) to it, and then turns the port number into a string so that it can be concatinated to the rest of the address. Stores in address.
 	conn, err := net.DialTimeout(protocol, address, 60*time.Second) /* DialTimeout is a function in the net package. It takes a protocol, address, and a timeout duration. It attempts to connect to the address
