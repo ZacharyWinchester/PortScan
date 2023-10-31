bd1727cc9552a7781aa436394ae6cac0250238de
ZacharyWinchester <zdnewinchester@gmail.com>
2023-10-30 20:35:40 -0400

    Update port.go

--- Port/port.go
+++ Port/port.go
@@ -4,7 +4,6 @@ import (
 	"net" // Golang portable interface for network input/output.
 	"strconv" // Implements conversions to and from string representations of basic data types.
 	"time" // Provides functionality for measuring and displaying time.
-	"sync"
 	"sort"
 	log "github.com/sirupsen/logrus" // Adds advanced logging functionality using the logrus package.
 )
@@ -15,10 +14,34 @@ type ScanResult struct { // Creates a structure to be called later for impliment
 	State string
 }
 
+type T = interface{} // Accepts any type
+
+type WorkerPool interface { // Contract for Worker Pool implementation
+	Run()
+	AddTask(task func())
+}
+
+type workerPool struct {
+	maxWorker int
+	queuedTask C chan func()
+}
+
+func (wp *workerPool) Run() {// This is the run method as detailed in the above interface.
+	for i := 0; i < wp.maxWorker; i++ { // Spawns a number of goroutines based on the number of max workers.
+		go func(workerID int) {
+			for task := range wp.queuedTaskC {
+				task()
+			}
+		}(i+1)
+	}
+}
+
+func (wp *workerPool) AddTask(task func()) {
+	wp.queuedTaskC <- task // Push task to queuedTaskC channel.
+}
 var (
 	mutex sync.Mutex
-	wg sync.WaitGroup
-	results []ScanResult // Initalizes a zero-filled array in results
+	results := ScanResults()
 	ClosedCounter int
 )
 
@@ -45,23 +68,29 @@ func ScanPort(protocol, hostname string, port int) ScanResult { // Function that
 }
 
 func InitialScan(hostname string) []ScanResult { // Takes an IP address as an argument, and returns an array
-	for i := 1; i <= 60000; i++ { // As long as i is less than or equal to 1024, run the following and increase i by one.
-		wg.Add(1)
-		go func(port int) {
+	totalWorker := 10 // Creates 10 workers
+	wp := workerpool.NewWorkerPool(totalWorker) // Creates a pool of workers
+	wp.Run() // Calls the run function
+
+	totalTask := 60000
+	resultC := make(chan ScanResult, totalTask) // Makes the result channel with the size of totalTask
+	
+	for i := 1; i <= totalTask; i++ { // As long as i is less than or equal to 1024, run the following and increase i by one.
+		wp.AddTask(func(port int) {
 			defer wg.Done()
 			result := ScanPort("tcp", hostname, port)
 			if result.State == "Open" {
 				mutex.Lock()
-				results = append(results, result)
+				resultC <- ScanResult{result}
 				mutex.Unlock()
 			} else {
 				mutex.Lock()
 				ClosedCounter++
 				mutex.Unlock()
 			}
-		}(i)
+		}(i))
 	}
-	wg.Wait()
+	<-waitC
 	sort.SliceStable(results, func(i, j int) bool {
 		return results[i].Port < results[j].Port
 	})
